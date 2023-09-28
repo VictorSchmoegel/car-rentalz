1076a2ce075e573026c99a33a3875bbf7724258e
Victor Schmoegel <victorschmoegel@gmail.com>
2023-09-28 14:57:14 -0300

    criada a pag testimonials e iniciada a estilização

--- car-rentalz/src/App.js
+++ car-rentalz/src/App.js
@@ -4,6 +4,7 @@ import Header from './components/Header';
 import Home from './pages/Home';
 import About from './pages/About';
 import Models from './pages/Models';
+import Testimonials from './pages/Testimonials/Testimonials';
 
 function App() {
   return (
@@ -13,6 +14,7 @@ function App() {
           <Route index path="/" element={<Home />} />
           <Route path="/about" element={<About />} />
           <Route path="/models" element={<Models />} />
+          <Route path="/testimonials" element={<Testimonials />} />
         </Routes>
       </div>
   );
